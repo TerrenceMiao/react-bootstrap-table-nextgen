4ae40b318d9056c45b80f6f26244494f639a1631
Terrence Miao <terrence.miao@gmail.com>
2023-08-12 19:36:39 +1000

    Fix eslint errors

--- packages/react-bootstrap-table-nextgen/test/test-helpers/mock-component.ts
+++ packages/react-bootstrap-table-nextgen/test/test-helpers/mock-component.ts
@@ -1,8 +1,8 @@
-import React from "react";
+import { Component } from "react";
 
-const extendTo =
-  <T extends React.ComponentType<any>>(Base: T) =>
-    (props: React.ComponentProps<T>) =>
-      null;
-
-export default extendTo;
+export const extendTo = (Base: any) =>
+  class MockComponent extends Component<typeof Base> {
+    render() {
+      return null;
+    }
+  };
