b962f35f20fd4eb07397bcdf898ed75f01e1d8b5
Terrence Miao <terrence.miao@gmail.com>
2023-08-29 09:34:00 +1000

    Work around "findDOMNode is deprecated in StrictMode" issue in react-transition-group and release react-bootstrap-table-nextgen version 4.3.4

--- packages/react-bootstrap-table-nextgen/src/row-expand/expand-row.tsx
+++ packages/react-bootstrap-table-nextgen/src/row-expand/expand-row.tsx
@@ -17,36 +17,41 @@ const ExpandRow: React.FC<ExpandRowProps> = ({
   onClosed,
   className,
   ...rest
-}) => (
-  <tr>
-    <td className={ cs("reset-expansion-style", className) } { ...rest }>
-      <CSSTransition
-        appear
-        in={ expanded }
-        timeout={ 400 }
-        classNames="row-expand-slide"
-        onExited={ onClosed }
-      >
-        <div>
-          <div className="row-expansion-style">{children}</div>
-        </div>
-      </CSSTransition>
-    </td>
-  </tr>
-);
+}) => {
+  const nodeRef = React.useRef(null);
+
+  return (
+    <tr>
+      <td className={cs("reset-expansion-style", className)} {...rest}>
+        <CSSTransition
+          appear
+          in={expanded}
+          timeout={400}
+          classNames="row-expand-slide"
+          onExited={onClosed}
+          nodeRef={nodeRef}
+        >
+          <div ref={nodeRef}>
+            <div className="row-expansion-style">{children}</div>
+          </div>
+        </CSSTransition>
+      </td>
+    </tr>
+  );
+};
 
 ExpandRow.propTypes = {
   children: PropTypes.node,
   expanded: PropTypes.bool,
   onClosed: PropTypes.func,
-  className: PropTypes.string
+  className: PropTypes.string,
 };
 
 ExpandRow.defaultProps = {
   children: null,
   expanded: false,
   onClosed: undefined,
-  className: ""
+  className: "",
 };
 
 export default ExpandRow;
