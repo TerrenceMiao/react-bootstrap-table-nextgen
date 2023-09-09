646432e8a76435e36f124c1c893b8f3ceb3f526a
Terrence Miao <terrence.miao@gmail.com>
2023-09-10 08:10:51 +1000

    Make all stories for Bootstrap 4 storybook work

--- packages/react-bootstrap-table-nextgen-example/src/stories/Bootstrap4.stories.tsx
+++ packages/react-bootstrap-table-nextgen-example/src/stories/Bootstrap4.stories.tsx
@@ -1,7 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 // import bootstrap style by given version
-import { productsGenerator, sortColumns } from '../utils/common';
+import paginationFactory from "../../../react-bootstrap-table-nextgen-paginator";
+import { columns, productsGenerator, sortColumns } from '../utils/common';
 import BootstrapTable from './Bootstrap4';
 import bootstrapStyle, { BOOTSTRAP_VERSION } from './bootstrap-style';
 
@@ -21,7 +22,7 @@ const meta = {
     columns: { control: 'object', description: 'table columns' },
     sourceCode: { control: 'text', description: 'source code of the table' },
     defaultSorted: { control: 'object', description: 'default sorted data field' },
-
+    selectRow: { control: 'object', description: 'table select row' },
   },
   decorators: [
     (Story: any) => bootstrapStyle(BOOTSTRAP_VERSION.FOUR)(Story),
@@ -74,5 +75,194 @@ export const SortTableWithBootstrap4: Story = {
     />
     `,
     defaultSorted: defaultSorted,
+  }
+};
+
+export const TableCaptionBootstrap4: Story = {
+  name: "Table caption Bootstrap 4",
+  args: {
+    mode: "caption",
+    columns: columns,
+    data: productsGenerator(),
+    sourceCode: `\
+    import BootstrapTable from 'react-bootstrap-table-nextgen';
+
+    const columns = [{
+      dataField: 'id',
+      text: 'Product ID'
+    }, {
+      dataField: 'name',
+      text: 'Product Name'
+    }, {
+      dataField: 'price',
+      text: 'Product Price'
+    }];
+
+    const CaptionElement = () => <h3 style={{ borderRadius: '0.25em', textAlign: 'center', color: 'purple', border: '1px solid purple', padding: '0.5em' }}>Component as Header</h3>;
+
+    <BootstrapTable bootstrap4 keyField="id" data={ products } caption="Plain text header" columns={ columns } />
+
+    <BootstrapTable bootstrap4 keyField="id" data={ products } caption={<CaptionElement />} columns={ columns } />
+    `,
+  }
+};
+
+const selectRow = {
+  mode: 'radio',
+  clickToSelect: true
+};
+
+export const RowSelectionTableWithBootstrap4: Story = {
+  name: "Row selection table with Bootstrap 4",
+  args: {
+    columns: columns,
+    data: productsGenerator(),
+    sourceCode: `\
+    import BootstrapTable from 'react-bootstrap-table-nextgen';
+
+    const columns = [{
+      dataField: 'id',
+      text: 'Product ID'
+    }, {
+      dataField: 'name',
+      text: 'Product Name'
+    }, {
+      dataField: 'price',
+      text: 'Product Price'
+    }];
+
+    const selectRow = {
+      mode: 'radio',
+      clickToSelect: true
+    };
+
+    <BootstrapTable
+      bootstrap4
+      keyField='id'
+      data={ products }
+      columns={ columns }
+      selectRow={ selectRow }
+    />
+    `,
+    selectRow: selectRow,
+  }
+};
+
+export const PaginationTableWithBootstrap4: Story = {
+  name: "Pagination table with Bootstrap 4",
+  args: {
+    columns: columns,
+    data: productsGenerator(87),
+    sourceCode: `\
+    import BootstrapTable from 'react-bootstrap-table-nextgen';
+    import paginationFactory from 'react-bootstrap-table-nextgen-paginator';
+
+    const columns = [{
+      dataField: 'id',
+      text: 'Product ID'
+    }, {
+      dataField: 'name',
+      text: 'Product Name'
+    }, {
+      dataField: 'price',
+      text: 'Product Price'
+    }];
+
+    <BootstrapTable bootstrap4 keyField='id' data={ products } columns={ columns } pagination={ paginationFactory() } />
+    `,
+    pagination: paginationFactory(),
+  }
+};
+
+export const ColumnToggleWithBootstrap4: Story = {
+  name: "Columns toggle with Bootstrap 4",
+  args: {
+    mode: "toggle",
+    columns: columns,
+    data: productsGenerator(),
+    sourceCode: `\
+    import BootstrapTable from 'react-bootstrap-table-nextgen';
+    import ToolkitProvider, { ColumnToggle } from 'react-bootstrap-table-nextgen-toolkit';
+
+    const { ToggleList } = ColumnToggle;
+    const columns = [{
+      dataField: 'id',
+      text: 'Product ID'
+    }, {
+      dataField: 'name',
+      text: 'Product Name'
+    }, {
+      dataField: 'price',
+      text: 'Product Price'
+    }];
+
+    <ToolkitProvider
+      keyField="id"
+      data={ products }
+      columns={ columns }
+      columnToggle
+    >
+      {
+        props => (
+          <div>
+            <ToggleList { ...props.columnToggleProps } />
+            <hr />
+            <BootstrapTable
+              { ...props.baseProps }
+            />
+          </div>
+        )
+      }
+    </ToolkitProvider>
+    `,
+  }
+};
+
+export const ToolkitsTableBootstrap4: Story = {
+  name: "Toolkits table Bootstrap 4",
+  args: {
+    mode: "toolkits",
+    columns: columns,
+    data: productsGenerator(),
+    sourceCode: `\
+    import BootstrapTable from 'react-bootstrap-table-nextgen';
+    import ToolkitProvider, { Search, CSVExport } from 'react-bootstrap-table-nextgen-toolkit';
+
+    const { SearchBar, ClearSearchButton } = Search;
+    const { ExportCSVButton } = CSVExport;
+
+    const columns = [{
+      dataField: 'id',
+      text: 'Product ID'
+    }, {
+      dataField: 'name',
+      text: 'Product Name'
+    }, {
+      dataField: 'price',
+      text: 'Product Price'
+    }];
+
+    <ToolkitProvider
+      keyField="id"
+      data={ products }
+      columns={ columns }
+      search
+    >
+      {
+        props => (
+          <div>
+            <h3>Input something at below input field:</h3>
+            <SearchBar { ...props.searchProps } />
+            <ClearSearchButton { ...props.searchProps } />
+            <hr />
+            <BootstrapTable
+              { ...props.baseProps }
+            />
+            <ExportCSVButton { ...props.csvProps }>Export CSV!!</ExportCSVButton>
+          </div>
+        )
+      }
+    </ToolkitProvider>
+    `,
   }
 };
--- packages/react-bootstrap-table-nextgen-example/src/stories/Bootstrap4.tsx
+++ packages/react-bootstrap-table-nextgen-example/src/stories/Bootstrap4.tsx
@@ -1,18 +1,107 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
 
-import Code from "../components/common/code-block";
 import BootstrapTable from "../../../react-bootstrap-table-nextgen";
+import ToolkitProvider, {
+  CSVExport,
+  ColumnToggle,
+  Search,
+} from "../../../react-bootstrap-table-nextgen-toolkit";
+import Code from "../components/common/code-block";
 
-import "./stylesheet/storybook.scss";
-import "./stylesheet/tomorrow.min.css";
 import "../../../react-bootstrap-table-nextgen-filter/style/react-bootstrap-table-nextgen-filter.scss";
 import "../../../react-bootstrap-table-nextgen-paginator/style/react-bootstrap-table-nextgen-paginator.scss";
 import "../../../react-bootstrap-table-nextgen-toolkit/style/react-bootstrap-table-nextgen-toolkit.scss";
 import "../../../react-bootstrap-table-nextgen/style/react-bootstrap-table-nextgen.scss";
+import "./stylesheet/storybook.scss";
+import "./stylesheet/tomorrow.min.css";
 
-export default ({ mode, data, columns, sourceCode, defaultSorted }) => {
+export default ({
+  mode,
+  data,
+  columns,
+  sourceCode,
+  defaultSorted,
+  selectRow,
+  pagination,
+}) => {
   switch (mode) {
+    case "caption":
+      const Caption = () => (
+        <h3
+          style={{
+            borderRadius: "0.25em",
+            textAlign: "center",
+            color: "purple",
+            border: "1px solid purple",
+            padding: "0.5em",
+          }}
+        >
+          Component as Header
+        </h3>
+      );
+      return (
+        <div>
+          <BootstrapTable
+            bootstrap4
+            keyField="id"
+            data={data}
+            caption="Plain text header"
+            columns={columns}
+          />
+          <BootstrapTable
+            bootstrap4
+            keyField="id"
+            data={data}
+            caption={<Caption />}
+            columns={columns}
+          />
+          <Code>{sourceCode}</Code>
+        </div>
+      );
+    case "toggle":
+      const { ToggleList } = ColumnToggle;
+      return (
+        <div>
+          <ToolkitProvider
+            keyField="id"
+            data={data}
+            columns={columns}
+            columnToggle
+          >
+            {(props) => (
+              <div>
+                <ToggleList {...props.columnToggleProps} />
+                <hr />
+                <BootstrapTable {...props.baseProps} />
+              </div>
+            )}
+          </ToolkitProvider>
+          <Code>{sourceCode}</Code>
+        </div>
+      );
+    case "toolkits":
+      const { SearchBar, ClearSearchButton } = Search;
+      const { ExportCSVButton } = CSVExport;
+      return (
+        <div>
+          <ToolkitProvider keyField="id" data={data} columns={columns} search>
+            {(props) => (
+              <div>
+                <h3>Input something at below input field:</h3>
+                <SearchBar {...props.searchProps} />
+                <ClearSearchButton {...props.searchProps} />
+                <hr />
+                <BootstrapTable {...props.baseProps} />
+                <ExportCSVButton {...props.csvProps}>
+                  Export CSV!!
+                </ExportCSVButton>
+              </div>
+            )}
+          </ToolkitProvider>
+          <Code>{sourceCode}</Code>
+        </div>
+      );
     default:
       return (
         <div>
@@ -22,6 +111,8 @@ export default ({ mode, data, columns, sourceCode, defaultSorted }) => {
             data={data}
             columns={columns}
             defaultSorted={defaultSorted}
+            selectRow={selectRow}
+            pagination={pagination}
           />
           <Code>{sourceCode}</Code>
         </div>
