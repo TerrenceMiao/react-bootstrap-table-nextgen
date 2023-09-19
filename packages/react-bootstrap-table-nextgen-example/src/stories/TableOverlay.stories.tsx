0701f7a78d05b476c80629aaf2b385e17b9a52bc
Terrence Miao <terrence.miao@gmail.com>
2023-09-20 02:01:56 +1000

    Make all stories for Table Overlay storybook work

--- packages/react-bootstrap-table-nextgen-example/src/stories/TableOverlay.stories.tsx
+++ packages/react-bootstrap-table-nextgen-example/src/stories/TableOverlay.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 // import bootstrap style by given version
-import BootstrapTable from './EmptyTableOverlay';
+import BootstrapTable from './TableOverlay';
 import bootstrapStyle from './bootstrap-style';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
